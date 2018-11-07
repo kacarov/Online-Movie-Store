de90ecdeb11166f709849281cb8837a5a3e9be52
Ioan Stavrev <j.stavrev@yahoo.com>
2018-11-07 08:07:24 +0200

    - implemented AddGenre

---
+++ OnlineMovieStore/OnlineMovieStore.Web/Areas/Administration/Models/AddGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineMovieStore.Web.Areas.Administration.Models
+{
+    public class AddGenreViewModel
+    {
+        public string Name { get; set; }
+    }
+}
