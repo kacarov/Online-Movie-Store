c656820e7fd3ad948b1b978491d140435a5f92a4
Stefan Veselinov <chofexx@gmail.com>
2018-11-08 19:35:19 +0200

    Demo concept of displaying deposit -! DEPOSIT DOESN'T WORK

---
+++ OnlineMovieStore/OnlineMovieStore.Web/Models/UserDepositViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlineMovieStore.Models.Models;
+
+namespace OnlineMovieStore.Web.Models
+{
+    public class UserDepositViewModel
+    {
+        public UserDepositViewModel(ApplicationUser user)
+        {
+            this.Balance = user.Balance;
+            this.UserName = user.UserName;
+        }
+
+        public double Balance { get; set; }
+        public string UserName { get; set; }
+    }
+}
